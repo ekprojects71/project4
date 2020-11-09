4d7c31261103fef942a12c026f18b2fb2133f5df
ekprojects71 <60908597+ekprojects71@users.noreply.github.com>
2020-11-08 20:07:12 -0600

    rebuilt app, updated dist folder

---
+++ backend/dist/precache-manifest.6f320643f6b55235d6a19a2c1db51795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b55999f61e01989632e4f4cad35401",
+    "url": "./index.html"
+  },
+  {
+    "revision": "457b262f9dc7e6c6057d",
+    "url": "./static/css/main.8c30b30e.chunk.css"
+  },
+  {
+    "revision": "b313504b060c4b4c5931",
+    "url": "./static/js/2.3ef7a737.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.3ef7a737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457b262f9dc7e6c6057d",
+    "url": "./static/js/main.08c857ec.chunk.js"
+  },
+  {
+    "revision": "3f89bf85507742ecb86b",
+    "url": "./static/js/runtime-main.870d044f.js"
+  }
+]);
